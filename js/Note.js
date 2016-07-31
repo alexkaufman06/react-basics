dc340c96e9ede9ae46345fc703af8226892a402d
Alex Kaufman <alexkaufman06@gmail.com>
2016-07-30 21:34:42 -0700

    Add refs, propTypes, and multiple child elements

--- js/Note.js
+++ js/Note.js
@@ -1,4 +1,4 @@
-// Chapter 4 is NEXT
+// 4.4 is NEXT
 
 var Note = React.createClass({
   getInitialState: function() {
@@ -8,6 +8,8 @@ var Note = React.createClass({
     this.setState({editing: true});
   },
   save: function() {
+    var val = this.refs.newText.getDOMNode().value;
+    alert("New note:" + val);
     this.setState({editing: false});
   },
   remove: function() {
@@ -27,7 +29,7 @@ var Note = React.createClass({
   renderForm: function() {
     return (
       <div className="note">
-        <textarea className="form-control" defaultValue={this.props.children}></textarea>
+        <textarea ref="newText" className="form-control" defaultValue={this.props.children}></textarea>
         <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
       </div>
     );
@@ -41,6 +43,41 @@ var Note = React.createClass({
   }
 });
 
-React.render(<Note>Hello React!</Note>,
+var Board = React.createClass({
+  propTypes: {
+    count: function(props, propName) {
+      if (typeof prop[propName] !== "number") {
+        return new Error('The count property must be a number!');
+      }
+      if (prop[propName] > 100) {
+        return new Error('Creating ' + prop[propName] + ' note is ridonculous');
+      }
+    }
+  },
+  getInitialState: function() {
+    return {
+      // object with key of notes
+      notes: [
+        'Clean toilet',
+        'Daily bowel movement',
+        'Feed Mr. Bojangles',
+        'Weave baskets'
+      ]
+    };
+  },
+  render: function() {
+    return (
+      <div className="board">
+        {this.state.notes.map(function(note, i){
+          return (
+            <Note key={i}>{note}</Note>
+          );
+        })}
+      </div>
+    );
+  }
+});
+
+React.render(<Board count={10}/>,
   document.getElementById('react-container')
 );
